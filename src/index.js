564feb50b70ab9d29078b51848e5b7a5d806d963
Maksim Palonski <mchp010nsky@gmail.com>
2018-02-25 00:52:48 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arrBracket = str.split('');
+    let checkFlag = false;
+    let count = 0;
+
+    for( let k = 0; k < arrBracket.length; k++ ) {
+        if( count < 0 ) return false;
+        for (let i = 0; i < bracketsConfig.length; ) {
+            if( arrBracket[k] === bracketsConfig[i][0] ){
+                count ++;
+                break;
+            } else if( arrBracket[k] === bracketsConfig[i][1] ){
+                count --;
+                break;
+            } else i++;
+        }
+    }
+
+    return count === 0;
 }
